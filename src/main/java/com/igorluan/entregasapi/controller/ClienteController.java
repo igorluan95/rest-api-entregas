ec1df150356fe57e3b4b98238610fa23a94d790a
igorluan95 <igorluansouzasilva@gmail.com>
2022-05-27 21:45:05 -0300

    Implementação do CRUD de cliente e do versionamento de Banco de Dados utilizando Flyway

--- src/main/java/com/igorluan/entregasapi/controller/ClienteController.java
+++ src/main/java/com/igorluan/entregasapi/controller/ClienteController.java
@@ -1,9 +1,72 @@
 package com.igorluan.entregasapi.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.igorluan.entregasapi.domain.model.Cliente;
+import com.igorluan.entregasapi.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
+@RequestMapping("/clientes")
 
 public class ClienteController {
 
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
+	public List<Cliente> listarClientes() {
+		return clienteRepository.findAll();
+	}
+
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscarClienteId(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionarClientes (@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizarCliente (@PathVariable Long clienteId,
+			@RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> deletarCliente (@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(clienteId);
+		return ResponseEntity.noContent().build(); 
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/com/igorluan/entregasapi/domain/model/Cliente.java
+++ src/main/java/com/igorluan/entregasapi/domain/model/Cliente.java
@@ -1,7 +1,36 @@
 package com.igorluan.entregasapi.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+@Table(name= "cliente")
+
 public class Cliente {
 	
-
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="nome")	
+	private String nome;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="fone")
+	private String telefone;
 
 }
